6823b471bc064086930c1fe4e24c4753ba21d17e
khcho96 <khcho1492@gmail.com>
2025-10-23 19:06:26 +0900

    refactor(OutputView): 메서드 static 지정 printCarNamesRequest 메서드 static으로 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 public class OutputView {
     private static final String CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public void printCarNamesRequest() {
+    public static void printCarNamesRequest() {
         System.out.println(CAR_NAMES_REQUEST_MESSAGE);
     }
 }
