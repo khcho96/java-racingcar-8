3ecee8c1dc73bff0f0c6c5ebb1075e6daa24139f
khcho96 <khcho1492@gmail.com>
2025-10-23 19:30:40 +0900

    feat(InputParser): 객체 생성 기능 추가 및 입력값 검증 메서드 호출 코드 추가 생성자 및 from 메서드 생성 입력값이 null 또는 빈 문자열(공백만 존재하는 경우 포함)이면 에외를 발생시키는 Validator의 메서드 호출하는 코드 추가

---
+++ src/main/java/racingcar/model/InputParser.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+import racingcar.util.Validator;
+
+public class InputParser {
+
+    private static InputParser inputParserObj;
+    private String input;
+
+    private InputParser(String input) {
+        this.input = input;
+    }
+
+    public static InputParser from(String input) {
+        Validator.validateNullOrBlank(input);
+        return new InputParser(input);
+    }
+}
