e0ae9a4637e4dfa3d79156d477aa4d1681e8eb03
khcho96 <khcho1492@gmail.com>
2025-10-24 21:38:41 +0900

    refactor(Converter): 오버 엔지니어링 제거 Validator에서 입력이 양수가 아닌 경우를 모두 처리하므로 parseUnsignedInt 대신 parseInt 사용해도 무방

--- src/main/java/racingcar/util/Converter.java
+++ src/main/java/racingcar/util/Converter.java
@@ -3,7 +3,7 @@ package racingcar.util;
 public class Converter {
     public static Integer convertStringToNumber(String input) {
         try {
-            return Integer.parseUnsignedInt(input);
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 시도할 횟수는 자연수만 가능합니다.");
         }
