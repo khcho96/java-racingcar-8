8a8ccff2bb87021f6c626e11a5fbb08ebe11ef64
khcho96 <khcho1492@gmail.com>
2025-10-24 19:50:13 +0900

    test(CarTest): 유효하지 않은 자동차 이름 검증 기능 테스트 유효하지 않은 자동차 이름을 검증하는 기능에 대한 테스트 코드 추가

---
+++ src/test/java/racingcar/model/domain/CarTest.java
@@ -0,0 +1,24 @@
+package racingcar.model.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+    @DisplayName("자동차 이름 공백 검증")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    void validateCarNameNBlankTest(String carName) {
+        // given
+        String expectedMessage = "[ERROR] 유효하지 않은 입력값입니다.";
+
+        // when then
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessage);
+    }
+}
