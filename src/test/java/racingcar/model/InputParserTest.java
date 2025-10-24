5d70283b42bad4d343a743a7ad4f88dc0c713c3e
khcho96 <khcho1492@gmail.com>
2025-10-24 21:52:04 +0900

    test(InputParserTest): 시도할 횟수 입력값에 대한 검증 기능 테스트 시도할 횟수 입력값이 최댓값을 초과하면 예외 발생시키는 기능에 대한 테스트 코드 추가

--- src/test/java/racingcar/model/InputParserTest.java
+++ src/test/java/racingcar/model/InputParserTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -51,6 +52,20 @@ public class InputParserTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(expectedMessage);
     }
+
+    @DisplayName("시도할 횟수 입력값이 최댓값을 초과하면 예외 발생")
+    @Test
+    void roundInputExceedMaxTest() {
+        // given
+        String input = "10001";
+        String expectedMessage = "[ERROR] 시도할 횟수는 10,000 이하의 값만 가능합니다.";
+
+        // when then
+        assertThatThrownBy(()->InputParser.roundInputParserFrom(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(expectedMessage);
+    }
+
 
     @DisplayName("자동차 이름 쉼표로 분리")
     @ParameterizedTest
