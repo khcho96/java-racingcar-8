92231565f33bc2ac13134a057882d9e2f53afd5f
khcho96 <khcho1492@gmail.com>
2025-10-24 19:55:13 +0900

    feat(Cars): 자동차 이름 중복 검증 및 객체 생성 기능 추가 자동차 이름 중복에 대한 검증 기능 및 Car 리스트 생성

---
+++ src/main/java/racingcar/model/domain/Cars.java
@@ -0,0 +1,21 @@
+package racingcar.model.domain;
+
+import java.util.List;
+import racingcar.util.Validator;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars CarsFrom(List<String> carNames) {
+        Validator.validateUniqueCarName(carNames);
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .toList();
+        return new Cars(cars);
+    }
+}
