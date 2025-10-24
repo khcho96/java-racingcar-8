30eddf35ecb89a7f2f55d1805bb00c7484efd927
khcho96 <khcho1492@gmail.com>
2025-10-24 21:45:31 +0900

    feat(Constant): 자동차 이름 최대 길이 상수 처리

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
     public static final String DELIMITER = ",";
     public static final String NUMBER_PATTERN = "\\d+";
     public static final Integer MAX_ROUND = 10000;
+    public static final Integer MAX_CAR_NAME_LENGTH = 5;
 }
