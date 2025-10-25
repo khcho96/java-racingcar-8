c22b75a27709097ea5919b3b7f41035d0fbf8b18
khcho96 <khcho1492@gmail.com>
2025-10-25 09:20:28 +0900

    feat(RaceService): 입력 문자열로 자동차 리스트를 생성하는 메서드 생성

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,5 +1,13 @@
 package racingcar.service;
 
-public class RaceService {
+import java.util.List;
+import racingcar.model.InputParser;
+import racingcar.model.domain.Cars;
 
+public class RaceService {
+    public Cars generateCars(String carNames) {
+        InputParser inputParser = InputParser.carNamesInputParserFrom(carNames);
+        List<String> splitCarNames = inputParser.getSplitCarNames();
+        return Cars.CarsFrom(splitCarNames);
+    }
 }
