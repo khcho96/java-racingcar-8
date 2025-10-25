d9d5eb110ddaab0f573143eb7ee7543f858b131e
khcho96 <khcho1492@gmail.com>
2025-10-25 11:29:23 +0900

    feat(Validator): 에러메시지 상수처리로 인한 코드 수정

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -3,6 +3,11 @@ package racingcar.util;
 import static racingcar.constant.Constant.MAX_CAR_NAME_LENGTH;
 import static racingcar.constant.Constant.MAX_ROUND;
 import static racingcar.constant.Constant.NUMBER_PATTERN;
+import static racingcar.constant.ErrorMessage.CAR_NAME_LENGTH_ERROR;
+import static racingcar.constant.ErrorMessage.CAR_NAME_UNIQUE_ERROR;
+import static racingcar.constant.ErrorMessage.INVALID_ERROR;
+import static racingcar.constant.ErrorMessage.INVALID_ROUND_ERROR;
+import static racingcar.constant.ErrorMessage.ROUND_RANGE_ERROR;
 
 import java.util.List;
 
@@ -10,13 +15,13 @@ public class Validator {
 
     public static void validateNullOrBlank(String input) {
         if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력값입니다.");
+            throw new IllegalArgumentException(INVALID_ERROR.getErrorMessage());
         }
     }
 
     public static void validateCarNameLength(String CarName) {
         if (CarName.strip().length() > MAX_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름의 길이는 최대 5입니다.");
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR.getErrorMessage());
         }
     }
 
@@ -29,20 +34,20 @@ public class Validator {
                 .size();
 
         if (preLength != distinctLength) {
-            throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복을 허용하지 않습니다.");
+            throw new IllegalArgumentException(CAR_NAME_UNIQUE_ERROR.getErrorMessage());
         }
     }
 
     public static void validateNumber(String input) {
         if (!input.strip().matches(NUMBER_PATTERN) || input.strip().equals("0")) {
-            throw new IllegalArgumentException("[ERROR] 시도할 횟수는 자연수만 가능합니다.");
+            throw new IllegalArgumentException(INVALID_ROUND_ERROR.getErrorMessage());
         }
     }
 
     public static void validateExceedMax(String input) {
         int round = Converter.convertStringToNumber(input);
         if (round > MAX_ROUND) {
-            throw  new IllegalArgumentException("[ERROR] 시도할 횟수는 10,000 이하의 값만 가능합니다.");
+            throw  new IllegalArgumentException(ROUND_RANGE_ERROR.getErrorMessage());
         }
     }
 }
