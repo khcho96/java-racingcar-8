ddea9e2eecfd3136c5be8d5c0c7091162d997613
khcho96 <khcho1492@gmail.com>
2025-10-25 10:07:32 +0900

    feat(WinningCars): 우승자를 선정하는 기능을 가진 객체 생성 자동차 리스트를 받아 우승자를 선정하고 결과를 출력하는 문자열을 반환하는 메서드 생성

---
+++ src/main/java/racingcar/model/domain/WinningCars.java
@@ -0,0 +1,27 @@
+package racingcar.model.domain;
+
+import static racingcar.constant.Constant.RESULT_DELIMITER;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningCars {
+    private final List<Car> winningCars;
+
+    private WinningCars(List<Car> winningCars) {
+        this.winningCars = winningCars;
+    }
+
+    public static WinningCars winningCarsFrom(Cars cars) {
+        List<Car> winningCars = cars.getWinningCars();
+        return new WinningCars(winningCars);
+    }
+
+    public String getWinners() {
+        List<String> winnerList = new ArrayList<>();
+        for (Car winningCar : winningCars) {
+            winningCar.addTo(winnerList);
+        }
+        return String.join(RESULT_DELIMITER, winnerList);
+    }
+}
