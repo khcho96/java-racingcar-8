654e7f775a2101a96413ca92809aba75af353f92
khcho96 <khcho1492@gmail.com>
2025-10-25 10:28:55 +0900

    feat(Constant): 우승자 출력을 위한 쉼표 구분자 상수 처리

--- src/main/java/racingcar/constant/Constant.java
+++ src/main/java/racingcar/constant/Constant.java
@@ -2,6 +2,7 @@ package racingcar.constant;
 
 public class Constant {
     public static final String DELIMITER = ",";
+    public static final String RESULT_DELIMITER = ", ";
     public static final String NUMBER_PATTERN = "\\d+";
     public static final Integer MAX_ROUND = 10000;
     public static final Integer MAX_CAR_NAME_LENGTH = 5;
