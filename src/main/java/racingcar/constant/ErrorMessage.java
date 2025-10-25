c87f5c5143f7dd41b1b9c0ac872c30433d3ec4ed
khcho96 <khcho1492@gmail.com>
2025-10-25 21:19:05 +0900

    refactor(ErrorMessage): 상수명 변경
    INVALID_ERROR->INVALID_INPUT_ERROR

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package racingcar.constant;
 
 public enum ErrorMessage {
-    INVALID_ERROR("[ERROR] 유효하지 않은 입력값입니다."),
+    INVALID_INPUT_ERROR("[ERROR] 유효하지 않은 입력값입니다."),
     CAR_NAME_LENGTH_ERROR("[ERROR] 자동차 이름의 길이는 최대 5입니다."),
     CAR_NAME_UNIQUE_ERROR("[ERROR] 자동차 이름은 중복을 허용하지 않습니다."),
     INVALID_ROUND_ERROR("[ERROR] 시도할 횟수는 자연수만 가능합니다."),
