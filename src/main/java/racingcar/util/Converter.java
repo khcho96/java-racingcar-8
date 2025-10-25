4052f1101661c662554cda7e6c161d31b9415605
khcho96 <khcho1492@gmail.com>
2025-10-25 11:28:36 +0900

    feat(Conveter): 에러메시지 상수처리로 인한 코드 수정

--- src/main/java/racingcar/util/Converter.java
+++ src/main/java/racingcar/util/Converter.java
@@ -1,11 +1,13 @@
 package racingcar.util;
 
+import static racingcar.constant.ErrorMessage.INVALID_ROUND_ERROR;
+
 public class Converter {
     public static Integer convertStringToNumber(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 시도할 횟수는 자연수만 가능합니다.");
+            throw new IllegalArgumentException(INVALID_ROUND_ERROR.getErrorMessage());
         }
     }
 }
