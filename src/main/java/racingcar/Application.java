4e4185bb67e4cd7912722013090217ae21b81595
khcho96 <khcho1492@gmail.com>
2025-10-26 11:05:31 +0900

    comment(Application): TODO 주석 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,6 @@ import racingcar.service.RaceService;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         RaceController controller = new RaceController(
                 new RaceService()
         );
